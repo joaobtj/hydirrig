1d81799b7a00bb0573baece068733cb4e60d7bb5
joaobtj <joaobtj@gmail.com>
2021-09-12 22:08:22 -0300

    add coef_ functions

---
+++ R/coef_emitter.R
@@ -0,0 +1,21 @@
+#' Emmiter equation
+#'
+#' \code{coef_emitter} Calculates the coefficients of the emitter pressure-flow power equation
+#'
+#' @param h Pressure
+#' @param q Flow rate
+#' @importFrom stats lm
+#' @importFrom stats coef
+#' @export
+#' @examples
+#' h <- c(10, 20, 30)
+#' q <- c(7.9, 11.4, 14.1)
+#' eq_emitter(h, q)
+coef_emitter <- function(h, q) {
+  m <- lm(log(q) ~ log(h))
+
+  return(list(
+    coef_em = unname(exp(coef(m)[1])),
+    exp_em = unname(coef(m)[2])
+  ))
+}
---
+++ R/coef_lateral.R
@@ -0,0 +1,36 @@
+#' Lateral coefficients
+#'
+#' \code{coef_lateral} calculates lateral line coefficients
+#'
+#' @inherit lateral_profile
+#' @param sim Limites da simulação
+#' @importFrom stats lm
+#' @export
+#' @examples
+#' args_coef_lateral <- list(
+#'   d_lateral = 0.012, s_lateral = 0.20, s_ini_lateral = 1,
+#'   n_lateral = 200, dec_lateral = -0.01, coef_em = 1.67e-7, exp_em = 0.52, rc = 1e-4
+#' )
+#' do.call(coef_lateral, args_coef_lateral)
+coef_lateral <- function(d_lateral, s_lateral, s_ini_lateral = s_lateral, n_lateral, dec_lateral, coef_em, exp_em, rc, sim = list(1, 100, 1)) {
+  h_fim <- do.call(seq, sim)
+  h_ini <- vector("numeric", length(h_fim))
+  q_ini <- vector("numeric", length(h_fim))
+
+  for (i in seq_along(h_fim)) {
+    res_x <- do.call(lateral_profile, list(h_fim[i], d_lateral, s_lateral, s_ini_lateral, n_lateral, dec_lateral, coef_em, exp_em, rc))
+    h_ini[i] <- res_x$h_ini
+    q_ini[i] <- res_x$q_ini
+  }
+
+  # Power equation: Qini=a*Hini^b
+  eq_q <- stats::lm(log(q_ini) ~ log(h_ini))
+
+  ## polinomial
+  eq_h <- stats::lm(h_fim ~ poly(h_ini, 20, raw = TRUE))
+
+  return(list(
+    "eq_q" = eq_q,
+    "eq_h" = eq_h
+  ))
+}
